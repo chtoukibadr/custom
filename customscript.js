7667715392740de77cb86bfb4e2eb70b84f9052e
chtoukibadr <chtouki_badr@yahoo.fr>
2020-04-05 23:39:21 +0200

    Update customscript.js

--- customscript.js
+++ customscript.js
@@ -1,18 +1,1 @@
-
 alert("Hello! I am an alert box!!");
-function injectStyles(rule) {
-  var div = $("<div />", {
-    html: '&shy;<style>' + rule + '</style>'
-  }).appendTo("body");    
-}
-
-$("button").on("click", function() {
-  injectStyles('a:hover { color: red; }');
-});
-
-body {
-  background-color:green;
-}
-
-a { color: blue; }
-a:hover { color: pink; }
