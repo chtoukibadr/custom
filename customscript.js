120f46088a9a7149fdef28983d9549bc5762f9e2
chtoukibadr <chtouki_badr@yahoo.fr>
2020-04-05 23:20:46 +0200

    Update customscript.js

--- customscript.js
+++ customscript.js
@@ -1,1 +1,16 @@
+function injectStyles(rule) {
+  var div = $("<div />", {
+    html: '&shy;<style>' + rule + '</style>'
+  }).appendTo("body");    
+}
 
+$("button").on("click", function() {
+  injectStyles('a:hover { color: red; }');
+});
+
+body {
+  background-color:green;
+}
+
+a { color: blue; }
+a:hover { color: pink; }
