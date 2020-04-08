019191b0ffd60be93859c3c953648a05857cdc62
chtoukibadr <chtouki_badr@yahoo.fr>
2020-04-09 00:38:29 +0200

    Update customscript.js

--- customscript.js
+++ customscript.js
@@ -1,1 +1,8 @@
-alert("err");   
+alert("err");
+var elem = document.querySelector('#Link__link__3pNRT LoginLinks__link__1GboG');
+
+// Get HTML content
+var html = elem.innerHTML;
+
+// Set HTML content
+elem.innerHTML = 'Wsss';
