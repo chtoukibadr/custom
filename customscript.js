72f1930a9977fd4a9a1a9a116c237ff6920fdfed
chtoukibadr <chtouki_badr@yahoo.fr>
2020-04-09 00:34:21 +0200

    Update customscript.js

--- customscript.js
+++ customscript.js
@@ -1,8 +1,1 @@
-$(function(){ // .ready() callback, is only executed when the DOM is fully loaded
-        var length = $("p").length;
-        // The following will log 1 to the console, as the paragraph exists.
-        // This is the evidence that this method is only called when the
-        // DOM is fully loaded
-        console.log(length);
-        alert("ff");
-    });
+console.log("err");   
