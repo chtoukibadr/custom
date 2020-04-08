f45bc62d012b34a2165e43a26da272b81ead8367
chtoukibadr <chtouki_badr@yahoo.fr>
2020-04-08 02:55:05 +0200

    Update customscript.js

--- customscript.js
+++ customscript.js
@@ -1,5 +1,1 @@
-
-  $(document).ready(function() {
- document.getElementsByClassName('marketplace-lander-content').style.textAlign = 'left';}
-/*elems[0].innerHTML = '<span class="texte_countdown" style="text-align:center;">text</span>';*/
-
+  classes.marketplace-lander-content.style.textAlign= 'left';
