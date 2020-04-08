8863295a162355c2323d2e52daf94c7ce1e4cb3d
chtoukibadr <chtouki_badr@yahoo.fr>
2020-04-09 00:55:39 +0200

    Update customscript.js

--- customscript.js
+++ customscript.js
@@ -1,9 +1,3 @@
 alert("err");alert("ffldld");
 var elem = document.querySelector("label.input");
-console.log(elem);
-// Get HTML content
-var html = elem.innerHTML;
-
-// Set HTML content
-elem.innerHTML = 'Wsss';
-alert("ffldld");
+alert("ffldldhhh");
